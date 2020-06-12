48ef3a79c5a4f272ae5d353a05c41235c9c1b090
nikuzejl <nikuzejl@gmail.com>
2020-06-12 11:27:43 -0400

    Using firestore, can find address to firebase storage

---
+++ app/src/main/java/com/example/meddata/New_VisitAdapter.java
@@ -0,0 +1,89 @@
+package com.example.meddata;
+
+
+//import android.support.annotation.NonNull;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.firebase.ui.firestore.FirestoreRecyclerAdapter;
+import com.firebase.ui.firestore.FirestoreRecyclerOptions;
+import com.google.firebase.firestore.DocumentSnapshot;
+
+public class New_VisitAdapter extends FirestoreRecyclerAdapter<Visit, New_VisitAdapter.VisitHolder> {
+    private OnItemClickListener listener;
+
+    public New_VisitAdapter(@NonNull FirestoreRecyclerOptions<Visit> options) {
+        super(options);
+    }
+
+    @Override
+    protected void onBindViewHolder(@NonNull VisitHolder holder, int position, @NonNull Visit model) {
+        holder.textViewDate.setText(model.getDate());
+        holder.textViewHospital.setText(model.getHospital());
+        holder.textViewDoctor.setText(model.getDoctor());
+        holder.textViewTests.setText(model.getTests());
+        holder.textViewDiagnosis.setText(model.getDiagnosis());
+        holder.textViewTreatment.setText(model.getTreatment());
+        holder.textViewComments.setText(model.getComments());
+
+    }
+
+    @NonNull
+    @Override
+    public VisitHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.display_visit,
+                parent, false);
+        return new VisitHolder(v);
+    }
+
+    public void deleteItem(int position) {
+        getSnapshots().getSnapshot(position).getReference().delete();
+    }
+
+    class VisitHolder extends RecyclerView.ViewHolder {
+        TextView textViewDate;
+        TextView textViewDoctor;
+        TextView textViewHospital;
+        TextView textViewTests;
+        TextView textViewDiagnosis;
+        TextView textViewTreatment;
+        TextView textViewComments;
+
+
+        public VisitHolder(View itemView) {
+            super(itemView);
+            textViewDate = itemView.findViewById(R.id.date);
+            textViewDoctor = itemView.findViewById(R.id.doctor);
+            textViewHospital = itemView.findViewById(R.id.hospital);
+            textViewTests = itemView.findViewById(R.id.tests);
+            textViewDiagnosis = itemView.findViewById(R.id.diagnosis);
+            textViewTreatment = itemView.findViewById(R.id.treatment);
+            textViewComments = itemView.findViewById(R.id.comments);
+
+
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    int position = getAdapterPosition();
+                    if (position != RecyclerView.NO_POSITION && listener != null) {
+                        listener.onItemClick(getSnapshots().getSnapshot(position), position);
+                    }
+                }
+            });
+        }
+    }
+
+    public interface OnItemClickListener {
+        void onItemClick(DocumentSnapshot documentSnapshot, int position);
+    }
+
+    public void setOnItemClickListener(OnItemClickListener listener) {
+        this.listener = listener;
+    }
+}
--- app/src/main/java/com/example/meddata/Retrieve_Visit.java
+++ app/src/main/java/com/example/meddata/Retrieve_Visit.java
@@ -12,26 +12,39 @@ import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.ItemTouchHelper;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.firebase.ui.firestore.FirestoreRecyclerOptions;
 import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class Retrieve_Visit extends AppCompatActivity {
     private FirebaseDatabase db;
+      private FirebaseFirestore dbFS = FirebaseFirestore.getInstance();
     private DatabaseReference dbRef;
     private VisitAdapter visitAdapter;
+    private New_VisitAdapter new_visitAdapter;
     private ChildEventListener mChildEventListener;
+       private CollectionReference dbFScollections = dbFS.collection("forms");
 
     private ListView allVisits;
 
     ////
-    ListView listView;
-    TextView textView;
-    String[] listItem;
+//    ListView listView;
+//    TextView textView;
+//    String[] listItem;
 
     ///
 
@@ -39,6 +52,10 @@ public class Retrieve_Visit extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.retrieve_visit);
+
+        setUpRecyclerView();
+        ////keeeeep this
+  /*
         allVisits = (ListView) findViewById(R.id.all_visits);
 
         db = FirebaseDatabase.getInstance();
@@ -47,13 +64,6 @@ public class Retrieve_Visit extends AppCompatActivity {
         List<Visit> visits_list = new ArrayList<>();
         visitAdapter= new VisitAdapter(this, R.layout.retrieve_visit, visits_list);
         allVisits.setAdapter(visitAdapter);
-
-      //  ...........setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                displayVisit();
-//            }
-//        });
 
         if(mChildEventListener == null) {
             mChildEventListener = new ChildEventListener() {
@@ -88,6 +98,8 @@ public class Retrieve_Visit extends AppCompatActivity {
             }
         });
 
+  */
+
 ////////
 //        listView=(ListView)findViewById(R.id.all_visits);
 //        textView=(TextView)findViewById(R.id.textView);
@@ -108,10 +120,61 @@ public class Retrieve_Visit extends AppCompatActivity {
 //        //////
     }
 
-    public void displayVisit(String s){
-        Intent intent = new Intent(this, Display_Visit.class);
-        intent.putExtra("visitAddress", s);
-        startActivity(intent);
+    private void setUpRecyclerView() {
+        Query query = dbFScollections.orderBy("date", Query.Direction.DESCENDING);
+
+        FirestoreRecyclerOptions<Visit> options = new FirestoreRecyclerOptions.Builder<Visit>()
+                .setQuery(query, Visit.class)
+                .build();
+
+        new_visitAdapter = new New_VisitAdapter(options);
+
+        RecyclerView recyclerView = findViewById(R.id.recycler_view);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(new_visitAdapter);
+
+        new ItemTouchHelper(new ItemTouchHelper.SimpleCallback(0,
+                ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT) {
+            @Override
+            public boolean onMove(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder, RecyclerView.ViewHolder target) {
+                return false;
+            }
+
+            @Override
+            public void onSwiped(RecyclerView.ViewHolder viewHolder, int direction) {
+                new_visitAdapter .deleteItem(viewHolder.getAdapterPosition());
+            }
+        }).attachToRecyclerView(recyclerView);
+
+        new_visitAdapter.setOnItemClickListener(new New_VisitAdapter.OnItemClickListener() {
+            @Override
+            public void onItemClick(DocumentSnapshot documentSnapshot, int position) {
+                Visit visit = documentSnapshot.toObject(Visit.class);
+                String id = documentSnapshot.getId();
+                String path = documentSnapshot.getReference().getPath();
+                Toast.makeText(Retrieve_Visit.this,
+                        "Position: " + position + " ID: " + id, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        new_visitAdapter.startListening();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        new_visitAdapter.stopListening();
+    }
+
+//    public void displayVisit(String s){
+//        Intent intent = new Intent(this, Display_Visit.class);
+//        intent.putExtra("visitAddress", s);
+//        startActivity(intent);
+//    }
 }
 
